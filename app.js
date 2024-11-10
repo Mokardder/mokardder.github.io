cce9b6213f5eb2ad15e39dab8a06aeefb8757159
Mokardder <34837222+Mokardder@users.noreply.github.com>
2024-11-10 13:45:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 
-function postData() {
-  let name_input = document.getElementById('name').value;
-  let email_input = document.getElementById('email').value;
-  let msg_input = document.getElementById('msg').value;
-  let timestam = new Date().toLocaleString();
-
-
 const url = 'https://script.google.com/macros/s/AKfycbwyLYzWi4BFjxW46sfsaj54TZsvfsdclWBoRCwxSCQ_UYYZcj7CsnaBip40DWBUiqO0/exec';
 const data = { name: name_input, email: email_input, message: msg_input, type:"Feedback", date: timestam, site: "mokardder.000webhostapp.com"};
 
@@ -19,111 +12,15 @@ body: new URLSearchParams(data)
 .catch(error => console.error(error));
 }
 /* Get Location */
+// Get the full URL query string
+const queryString = window.location.search;
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition,showError);
-  } else {
-    console.log("Geolocation is not supported by this browser.");
-  }
-}
+// Parse the query string parameters
+const urlParams = new URLSearchParams(queryString);
 
-function showPosition(position) {
-  let location ="https://www.google.com/maps/?q="+position.coords.latitude+","+position.coords.longitude;
-  let browserIdentity = getBrowsername();
-  sendToGoogleSheet(location, browserIdentity);
-}
+// Get the value of the 'param' parameter
+const paramValue = urlParams.get('param');
 
-function showError(error) {
-  let errorValue = "";
-  let browserIdentity = getBrowsername();
-  
-  switch(error.code) {
-      
-    case error.PERMISSION_DENIED:
-       errorValue = "User denied the request for Geolocation.";
-      break;
-    case error.POSITION_UNAVAILABLE:
-      errorValue = "Location information is unavailable.";
-      break;
-    case error.TIMEOUT:
-      errorValue = "The request to get user location timed out.";
-      break;
-    case error.UNKNOWN_ERROR:
-      errorValue = "An unknown error occurred.";
-      break;
-  }
-  sendToGoogleSheet(errorValue, browserIdentity);
-}
-
-
-function sendToGoogleSheet( location, browserIdentity){
-
-  let timestam = new Date().toLocaleString(); 
-  
-  const url = 'https://script.google.com/macros/s/AKfycbwyLYzWi4BFjxW46sfsaj54TZsvfsdclWBoRCwxSCQ_UYYZcj7CsnaBip40DWBUiqO0/exec';
-  // const data = { name: browserIdentity, email: location, message: timestam+"\n\n--Visitor--\n\nmokardder.000webhostapp.com" };
-  
-  const data = { name: browserIdentity, email: location, message: "", type:"Visitor", date: timestam, site: "mokardder.000webhostapp.com"};
-
-
-
-  fetch(url, {
-  method: 'POST',
-  body: new URLSearchParams(data)
-  })
-  .then(response => response.text())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));    
-}
-
-
-
-function visitCount(){
-getLocation();
-}
-
-
-
-
-function getBrowsername(){
-const userAgentString = navigator.userAgent;
-const regex = /\([^)]+\)/;
-const match = userAgentString.match(regex);
-
-if (match) {
-
-  console.log(match[0]);
-} else {
-  console.log("No match found.");
-}
-let userAgent = match[0];
-return userAgent;
-}
-
-  function removePop(){
-            let popup  = document.getElementById("hide-pop");
-            popup.style.display = "none";
-              visitCount();
-        }
-        
-    
-
-window.onload = function() {
-            popUp();
-            
-        });
-    
-            
-            function popUp(){
-              let popup  = document.getElementById("hide-pop");
-              popup.style.display = "block";
- 
-            }
-        }
-      
-
-        
-
+console.log(paramValue); // This will output 'Mokaredder' if the URL is ?param=Mokaredder
 
  
